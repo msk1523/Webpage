d765453c61f48323de11dbdbe6d85b97c90d5dcf
Siva kumaran M <110098069+msk1523@users.noreply.github.com>
2023-10-22 01:42:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask , request
+import json
+
+app = Flask(__name__)
+
+count = 0
+
+@app.route('/')
+def Index():
+    return open("Index.html").read()
+
+@app.route('/log', methods=['POST'])
+def log_count():
+    global count
+    data = json.loads(request.data)
+    count = data['count']
+
+    # Log the count to a text file
+    with open('count_log.txt', 'a') as log_file:
+        log_file.write(f'Count - {count}\n')
+
+    return 'Logged'
+
+if __name__ == '__main__':
+    app.run(debug=True)
